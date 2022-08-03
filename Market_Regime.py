5ac1b5c94901f198be1d7005d53ef0ef8cdf1901
ibilalgo976 <110566030+ibilalgo976@users.noreply.github.com>
2022-08-03 23:40:41 +0300

    Add files via upload

--- Market_Regime.py
+++ Market_Regime.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
-import talib
+import talib as ta
 import streamlit as st
 from scipy.signal import argrelextrema
 
@@ -28,7 +28,7 @@ MACD_SLOW = 30
 MACD_SIGNAL = 5
 Y_AXIS_SIZE = 16
 df1 = yf.download(ticker,start='2021-01-01',progress=False)
-df1['macd'], df1['macdSignal'], df1['macdHist'] = talib.MACD(df1['Close'], fastperiod=MACD_FAST, slowperiod=MACD_SLOW, signalperiod=MACD_SIGNAL)
+df1['macd'], df1['macdSignal'], df1['macdHist'] = ta.MACD(df1['Close'], fastperiod=MACD_FAST, slowperiod=MACD_SLOW, signalperiod=MACD_SIGNAL)
 
 st.subheader("VIX")
 st.write(df1)
@@ -44,7 +44,7 @@ ax1.set_ylabel(ticker, size=20)
 #candlestick(ax1, df1, width=0.5, colorup='g', colordown='r', alpha=1)
 df1.Close.plot(ax=ax1, color='b', label='^VIX')
 
-# Draw MACD computed with Talib
+# Draw MACD computed with ta
 ax2.set_ylabel('MACD: '+ str(MACD_FAST) + ', ' + str(MACD_SLOW) + ', ' + str(MACD_SIGNAL), size=Y_AXIS_SIZE)
 df1.macd.plot(ax=ax2, color='b', label='Macd')
 df1.macdSignal.plot(ax=ax2, color='g', label='Signal')
@@ -62,15 +62,15 @@ df = yf.download('SPY',period='2y',progress=False,auto_adjust=True)
 #df = yf.download('SPY',start='2021-01-01',progress=False)
 #df_yahoo = yf.download('FB',start='2020-09-15',end='2020-11-15',progress=False,auto_adjust=True,actions="inline")
 df['Prev Close'] = df['Close'].shift(1)   
-df['MA200'] = talib.MA(df['Close'], timeperiod=200)
-df['MA50'] = talib.MA(df['Close'], timeperiod=50)
-df['ATR'] = talib.ATR(df['High'], df['Low'], df['Close'], timeperiod=14)
-df['ADX'] = talib.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
-df['WILLR3'] = talib.WILLR(df['High'], df['Low'], df['Close'], timeperiod=3)
-df['WILLR10'] = talib.WILLR(df['High'], df['Low'], df['Close'], timeperiod=10)
-df['WILLR30'] = talib.WILLR(df['High'], df['Low'], df['Close'], timeperiod=30)
-df['WILLR90'] = talib.WILLR(df['High'], df['Low'], df['Close'], timeperiod=90)
-df['WILLR270'] = talib.WILLR(df['High'], df['Low'], df['Close'], timeperiod=270)
+df['MA200'] = ta.MA(df['Close'], timeperiod=200)
+df['MA50'] = ta.MA(df['Close'], timeperiod=50)
+df['ATR'] = ta.ATR(df['High'], df['Low'], df['Close'], timeperiod=14)
+df['ADX'] = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
+df['WILLR3'] = ta.WILLR(df['High'], df['Low'], df['Close'], timeperiod=3)
+df['WILLR10'] = ta.WILLR(df['High'], df['Low'], df['Close'], timeperiod=10)
+df['WILLR30'] = ta.WILLR(df['High'], df['Low'], df['Close'], timeperiod=30)
+df['WILLR90'] = ta.WILLR(df['High'], df['Low'], df['Close'], timeperiod=90)
+df['WILLR270'] = ta.WILLR(df['High'], df['Low'], df['Close'], timeperiod=270)
 df['VIX Close'] = df1['Close']
 df['VIX Prev Close'] = df1['Close'].shift(1)
 df['VIX MACD'] = df1['macd']
@@ -91,7 +91,7 @@ df.loc[df['MID MKT STRENGTH Signal'] > 25,'MID MKT STRENGTH'] = 'STRONG'
 df.loc[df['MID MKT STRENGTH Signal'] < 15,'MID MKT STRENGTH'] = 'WEAK'
 
 df['MKT VOLATILTY Signal'] =  df['ATR'] / df['Close']
-df['BB3100Z1UP'], df['BB100Z1MID'], df['BB100Z1LO'] = talib.BBANDS(df['MKT VOLATILTY Signal'], timeperiod=100, nbdevup=1, nbdevdn=1, matype=0)
+df['BB3100Z1UP'], df['BB100Z1MID'], df['BB100Z1LO'] = ta.BBANDS(df['MKT VOLATILTY Signal'], timeperiod=100, nbdevup=1, nbdevdn=1, matype=0)
 df['MKT VOLATILTY'] = 'NORMAL'
 df.loc[df['MKT VOLATILTY Signal'] > df['BB3100Z1UP'],'MKT VOLATILTY'] = 'HIGH'
 df.loc[df['MKT VOLATILTY Signal'] < df['BB100Z1LO'] ,'MKT VOLATILTY'] = 'LOW'
