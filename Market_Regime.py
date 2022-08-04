0d243777e8ed355f4e4b3a5e4d20e91fcfe1abf3
ibilalgo976 <110566030+ibilalgo976@users.noreply.github.com>
2022-08-04 18:05:17 +0300

    Add files via upload

--- Market_Regime.py
+++ Market_Regime.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
-import talib as ta
+#import talib as ta
 import streamlit as st
 from scipy.signal import argrelextrema
 
